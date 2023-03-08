878d432e1196a4b8d448c48318b831f9a46a9dc8
Pizzafuglen <Crose19@student.sdu.dk>
2023-03-08 16:51:35 +0100

    Added the OpcUAWrite.cs class
    Signed-off-by: Pizzafuglen <Crose19@student.sdu.dk>

--- OpcUAWrite.cs
+++ OpcUAWrite.cs
@@ -1,5 +1,23 @@
-﻿namespace Sem3ReWrite
+﻿using Opc.UaFx.Client;
+
+namespace Sem3ReWrite
 {
-    public class OpcUAWrite
-    {}
+    public abstract class OpcUAWrite
+    {
+        private const string OpcUrl = "opc.tcp://127.0.0.1:4840";
+
+        public static void WriteNodeIntValue(string nodeId, int value)
+        {
+            var client = new OpcClient(OpcUrl);
+            client.Connect();
+            client.WriteNode(nodeId, value);
+        }
+
+        public static void WriteNodeFloatValue(string nodeId, float value)
+        {
+            var client = new OpcClient(OpcUrl);
+            client.Connect();
+            client.WriteNode(nodeId, value);
+        }
+    }
 }
